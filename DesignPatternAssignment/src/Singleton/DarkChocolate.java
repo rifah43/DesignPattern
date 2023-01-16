fd5440915b9ebd412c1f144d20af90b4c5f3b7cc
Rifah Tashfiha Faria <60043129+rifah43@users.noreply.github.com>
2023-01-17 01:27:50 +0600

    Delete DarkChocolate.java

--- DesignPatternAssignment/src/Singleton/DarkChocolate.java
+++
@@ -1,9 +0,0 @@
-package Singleton;
-
-public class DarkChocolate {
-    private float gramAmount;
-    public void addCocoa(float gramAmount){
-        this.gramAmount = gramAmount;
-        System.out.println(this.gramAmount+"gram Cocoa");
-    }
-}
