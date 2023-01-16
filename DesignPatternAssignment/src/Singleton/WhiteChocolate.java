18efcbc46a521bc5bd6de838aae273a22b4da81b
Rifah Tashfiha Faria <60043129+rifah43@users.noreply.github.com>
2023-01-17 01:28:31 +0600

    Delete WhiteChocolate.java

--- DesignPatternAssignment/src/Singleton/WhiteChocolate.java
+++
@@ -1,9 +0,0 @@
-package Singleton;
-
-public class WhiteChocolate {
-    private float litreAmount;
-    public void addMilk(float litreAmount){
-        this.litreAmount = litreAmount;
-        System.out.println(this.litreAmount+"litre milk");
-    }
-}
