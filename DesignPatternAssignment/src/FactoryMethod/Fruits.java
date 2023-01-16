19f97c456c6636fc49179ae6750c2f552ee568b0
Rifah Tashfiha Faria <60043129+rifah43@users.noreply.github.com>
2023-01-17 01:15:57 +0600

    Delete Fruits.java

--- DesignPatternAssignment/src/FactoryMethod/Fruits.java
+++
@@ -1,9 +0,0 @@
-package FactoryMethod;
-
-public class Fruits {
-    private int pieces;
-    public void mixFruits(int pieces){
-        this.pieces = pieces;
-        System.out.println(this.pieces+"pieces fruits");
-    }
-}
